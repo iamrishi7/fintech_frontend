a74b79deb105fb00a13faea080e1d29ff5428b45
sangam-cm <sangam@cloudmagician.in>
2024-04-10 19:58:18 +0530

    api url fix

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -8,7 +8,7 @@ export const CLIENT_BASE_URL =
 export const API_BASE_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:8000/api"
-    : "https://backend.janpay.in/api";
+    : "https://backend.janpay.online/api";
 
 export const STATES = [
   {
