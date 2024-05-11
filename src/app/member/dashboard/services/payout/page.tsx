1ad56aeaa1b5639e0e5750e474c329391fd4e061
DezyNation <dezynationindia@gmail.com>
2024-05-11 14:50:44 +0530

    default payout provider updated

--- src/app/member/dashboard/services/payout/page.tsx
+++ src/app/member/dashboard/services/payout/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
   const [formData, setFormData] = useState<any>(null);
   const [provider, setProvider] = useState<string | number | boolean>(
-    "eko"
+    "waayupay"
   );
   const [availableProviders, setAvailableProviders] = useState<any>([]);
 
