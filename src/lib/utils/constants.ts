d66610d30a35e39913c12c5c12915edf659de799
Rishi Kumar <108564869+iamrishi7@users.noreply.github.com>
2024-12-31 20:44:51 +0530

    Update constants.ts

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -8,7 +8,7 @@ export const CLIENT_BASE_URL =
 export const API_BASE_URL =
   process.env.NODE_ENV == "development"
     ? "http://localhost:8000/api"
-    : process.env.BACKEND_URL;
+    : process.env.API_URL;
 
 export const STATES = [
   {
