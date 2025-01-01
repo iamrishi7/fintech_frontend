9aa7f9b2006e4aae17dfcce5be0b9744a7abadb9
sangam-cm <sangam@cloudmagician.in>
2025-01-02 00:49:26 +0530

    groscope

--- src/app/member/dashboard/services/payout/page.tsx
+++ src/app/member/dashboard/services/payout/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
   const [formData, setFormData] = useState<any>(null);
   const [provider, setProvider] = useState<string | number | boolean>(
-    "safexpay"
+    "groscope"
   );
   const [availableProviders, setAvailableProviders] = useState<any>([]);
 
