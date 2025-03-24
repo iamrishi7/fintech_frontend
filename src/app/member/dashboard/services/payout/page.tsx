a6e7eac9203a054c65a673d902ccbdaf905cd8fb
sangam-cm <sangam@cloudmagician.in>
2025-03-24 19:29:33 +0530

    payscope

--- src/app/member/dashboard/services/payout/page.tsx
+++ src/app/member/dashboard/services/payout/page.tsx
@@ -30,7 +30,7 @@ const page = () => {
 
   const [formData, setFormData] = useState<any>(null);
   const [provider, setProvider] = useState<string | number | boolean>(
-    "groscope"
+    "payscope"
   );
   const [availableProviders, setAvailableProviders] = useState<any>([]);
 
